8cbbd065df1045dd281fe6904e4761e5a8603850
Vishwaas <vicky1352002@gmail.com>
2023-04-20 21:07:09 +0530

    Configure MongoDB with all Get,Post,Put,Delete

--- trainee_backend/controllers/goalController.js
+++ trainee_backend/controllers/goalController.js
@@ -1,9 +1,12 @@
 const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel');
 
 
 // Handles -Get Requests, Over Router - /api/goals 
-const getGoals = asyncHandler(async (req,res) => { 
-    res.status(200).json({message:'Get Goals'})
+const getGoals = asyncHandler(async (req,res) => {
+    
+    const goals = await Goal.find()
+    res.status(200).json({goals})
 })
 
 
@@ -13,20 +16,47 @@ const setGoals = asyncHandler(async (req,res) => {
 
     if(!req.body.text) {
         res.status(400)
-        throw new Error('Please add text field');
+        throw new Error('Please add text field inside SetGoal');
     }
-    res.status(200).json({message:'Set Goals'})
+
+    const goal = await Goal.create({
+        text : req.body.text
+    })
+
+    res.status(200).json({goal})
 })
 
 // Handles - Put Requests, Over Router - /api/goals 
 const updateGoals = asyncHandler(async (req,res) => { 
-    res.status(200).json({message:`Update Goals with Id ${req.params.id}`})
+    
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal Not Found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{
+        new : true
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 
 // Handles - Delete Requests, Over Router - /api/goals 
 const deleteGoals = asyncHandler(async (req,res) => { 
-    res.status(200).json({message:`Delete Goals with Id ${req.params.id}`})
+    
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal Not Found');
+    }
+    
+    await goal.deleteOne()
+
+    res.status(200).json({message:`Deleted Goals with Id ${req.params.id}`})
 })
 
 
--- trainee_backend/server.js
+++ trainee_backend/server.js
@@ -3,12 +3,15 @@ const dotenv = require('dotenv').config();
 const bodyParse = require('body-parser');
 const bodyParser = require('body-parser');
 const {errorHandler} = require('./middleware/errorMiddleware');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
 
 const port= process.env.PORT || 5000;
 
 
 const app = express();
-
+connectDB();
 // app.use(express.json);
 // app.use(express.urlencoded({extended:true}))
 
